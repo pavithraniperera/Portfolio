1cc1ec61313e345a26dc8b6c259ea834cfa80fe0
Pavithrani Perera <136662441+pavithraniperera@users.noreply.github.com>
2025-03-12 18:32:29 +0530

    Update StatInfoCard.jsx

--- src/Components/StatInfoCard.jsx
+++ src/Components/StatInfoCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const StatInfoCard = ({ count, label }) => {
   return (
-    <div className="flex-1 flex gap-3 md:gap-5 bg-gradient-to-b from-[#fcf4ec] to-[#ffffff] rounded-[14px] p-5">
+    <div className="flex-1 flex gap-3 md:gap-5 sm:bg-gradient-to-b from-[#fcf4ec] to-[#ffffff] rounded-[14px] p-5 bg-[#fcf4ec]">
       <h4 className="text-4xl md:text-5xl font-medium text-[#ac573f]">
         {count}
       </h4>
