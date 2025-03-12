60dd04b63b42dbc6a5bc30e4254b6c035a614e30
Pavithrani Perera <136662441+pavithraniperera@users.noreply.github.com>
2025-03-12 18:37:05 +0530

    Update ContactInfoCard.jsx

--- src/Components/ContactInfoCard.jsx
+++ src/Components/ContactInfoCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ContactInfoCard =({icon,text})=>{
     return (
         <div className="flex item-center gap-5 bg-orange-50 rounded border border-orange-100 px-4 py-3 mb-5">
-           <div className="w-10 h-10 text-white flex items-center justify-center bg-gradient-to-r from-[#f4a44f] to-[#ac573f] rounded-lg">{icon}</div>
+           <div className="w-10 h-10 text-white flex items-center justify-center sm:bg-gradient-to-r from-[#f4a44f] to-[#ac573f] rounded-lg bg-[#f4a44f]">{icon}</div>
            <p className="text-[#f4a44f] text-md md:text-sm">{text}</p>
         </div>
     )
